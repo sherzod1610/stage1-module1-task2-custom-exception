49bd65eef7cee3898ddef124bd804c0e1b97fe38
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-11 12:09:54 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,13 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException{
+    if (Student.getValueOf(studentID)==null){
+      return Student.getValueOf(studentID);
+    }else {
+      throw new StudentNotFoundException("Could not find student with ID " + studentID);
+    }
+
   }
 
   public static void main(String[] args) {
